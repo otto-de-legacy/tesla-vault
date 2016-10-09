c07c89fcc32d05549b38fac81bb22800a91bbfd6
carl.duevel <carl.duevel@holisticon.de>
2016-10-09 21:05:49 +0200

    lein-release plugin: bumped version from 1.0.1 to 1.0.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-vault "1.0.1"
+(defproject de.otto/tesla-vault "1.0.2-SNAPSHOT"
   :description "Read vault secrets in your edn config"
   :url "https://github.com/otto-de/tesla-vault"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,10 @@
   :scm {:name "git"
         :url  "https://github.com/otto-de/tesla-vault"}
   :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars
-                 :scm :git}
+  :lein-release {:scm :git
+                 :deploy-via :shell
+                 :shell ["lein" "deploy" "clojars"]
+                 }
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.3.0"]
                  [environ "1.1.0"]
