ffa28ab874b8cef0cd8f29592150e5a025f2cf22
Carl Düvel <carl.duevel@otto.de>
2017-06-19 11:54:45 +0200

    lein-release plugin: bumped version from 1.0.2 to 1.0.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-vault "1.0.2"
+(defproject de.otto/tesla-vault "1.0.3-SNAPSHOT"
   :description "Read vault secrets in your edn config"
   :url "https://github.com/otto-de/tesla-vault"
   :license {:name "Eclipse Public License"
